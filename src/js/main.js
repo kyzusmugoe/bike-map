e1ae17ec231b7ea3717783fa2b1bd03bf0129e46
Wade chen <kyzusmugoe@gmail.com>
2022-09-17 00:12:39 +0800

    完成追蹤跑者清單的功能

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 console.log("main")
 
+
+let runnerList = [];//追蹤的跑者清單
+
 document.addEventListener('DOMContentLoaded', () => {
-    
-
-
     const pages = document.querySelectorAll(".page")
     const btns = document.querySelectorAll(".pageBtn")
 
@@ -22,9 +22,43 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
     
+    const runnerAddHandler = ()=>{
+        let runnerList = [];
+        const boxUser = document.querySelector("#runnerBox")//user頁面
+        const boxSuggest = document.querySelector("#myRunnerList")//user頁面
+        const runnerName = document.querySelector("#runnerName")
+        
+        renderRunner = box =>{
+            while(box.firstChild){
+                box.removeChild(box.lastChild)
+            }    
+            runnerList.map(name=>{
+                let member = document.createElement("div")
+                member.innerHTML= name
+                box.appendChild(member)
+            })
+        }
+
+        document.querySelector("#addRunner").addEventListener("click",()=>{
+           
+            if(runnerName.value != ''){
+                runnerList.push(runnerName.value)
+                renderRunner(boxUser)
+                renderRunner(boxSuggest)
+                runnerName.value =""
+            }else{
+                alert("尚未填寫")
+            }
+            console.log(runnerList)
+        })
+
+        
+    }
+    
 
     //init
     closeAll()
     document.querySelector(`#user`).style.display = "block"
     setBtnsHandler()
+    runnerAddHandler()
 })
